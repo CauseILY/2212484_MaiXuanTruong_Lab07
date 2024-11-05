43746e1665c2ec9797cd62ccd015059cc918649e
PC819 <PC819@PC819>
2024-11-05 10:45:43 +0700

    Add project files.

---
+++ 2212484_MaiXuanTruong_Lab07/FoodInfoForm.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2212484_MaiXuanTruong_Lab07
+{
+    public partial class FoodInfoForm : Form
+    {
+        public FoodInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FoodInfoForm_Load(object sender, EventArgs e)
+        {
+            this.InitValues();
+        }
+        private void InitValues()
+        {
+            string connectionString = "Data Source=PC819;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ID, Name FROM Category";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            conn.Open();
+
+            adapter.Fill(ds,"Category");
+
+            cbbCatName.DataSource = ds.Tables["Category"];
+            cbbCatName.DisplayMember = "Name";  
+            cbbCatName.ValueMember = "ID";
+
+            conn.Close();
+            conn.Dispose(); 
+        }
+        private void ResetText()
+        {
+            txtFoodID.ResetText();
+            txtName.ResetText();
+            txtNotes.ResetText();
+            txtUnit.ResetText();
+            cbbCatName.ResetText();
+            nudPrice.ResetText();
+        }
+
+        private void btnAddFood_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "Data Source=PC819;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "EXECUTE InsertFood @id OUTPUT, @name, @unit, @foodcategoryID, @price, @notes";
+
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters.Add("@name", SqlDbType.NVarChar, 1000);
+                cmd.Parameters.Add("@unit", SqlDbType.NVarChar, 100);
+                cmd.Parameters.Add("@foodCategoryID", SqlDbType.Int);
+                cmd.Parameters.Add("@price", SqlDbType.Int);
+                cmd.Parameters.Add("@notes", SqlDbType.NVarChar, 3000);
+
+                cmd.Parameters["@id"].Direction= ParameterDirection.Output;
+
+                cmd.Parameters["@name"].Value = txtName.Text;
+                cmd.Parameters["@unit"].Value = txtUnit.Text;
+                cmd.Parameters["@foodCategoryID"].Value = cbbCatName.SelectedValue;
+                cmd.Parameters["@price"].Value = nudPrice.Value;
+                cmd.Parameters["@notes"].Value = txtNotes.Text;
+
+                conn.Open();
+
+                int numRowAffected = cmd.ExecuteNonQuery();
+                if (numRowAffected > 0)
+                {
+                    string foodID = cmd.Parameters["@id"].Value.ToString();
+                    MessageBox.Show("Successfully adding new food. Food ID= " + foodID, "Message");
+                    this.ResetText();
+                }
+                else
+                {
+                    MessageBox.Show("Adding food failed");
+                }
+                conn.Close();
+                conn.Dispose();
+            }
+            catch(SqlException exception)
+            {
+                MessageBox.Show(exception.Message, "SQL Error");
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+        public void DisplayFoodID(DataRowView rowView)
+        {
+            try
+            {
+                txtFoodID.Text = rowView["ID"].ToString();
+                txtName.Text = rowView["Name"].ToString();
+                txtUnit.Text = rowView["Unit"].ToString();
+                txtNotes.Text = rowView["Notes"].ToString();
+                nudPrice.Text = rowView["Price"].ToString();
+
+                cbbCatName.SelectedIndex = -1;
+                for(int index = 0; index <cbbCatName.Items.Count;index++)
+                {
+                    DataRowView cat = cbbCatName.Items[index] as DataRowView;
+                    if (cat["ID"].ToString() == rowView["FoodCategoryID"].ToString()) ;
+                    {
+                        cbbCatName.SelectedIndex = index;
+                        break;
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+                this.Close();
+            }
+        }
+
+        private void btnUpdateFood_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "Data Source=PC819;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "EXECUTE UpdateFood @id, @name, @unit, @foodCategoryID, @price, @notes";
+
+                // Thêm tham số vào đối tượng Command
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters.Add("@name", SqlDbType.VarChar, 1000);
+                cmd.Parameters.Add("@unit", SqlDbType.VarChar, 100);
+                cmd.Parameters.Add("@foodCategoryId", SqlDbType.Int);
+                cmd.Parameters.Add("@price", SqlDbType.Int);
+                cmd.Parameters.Add("@notes", SqlDbType.VarChar, 3000);
+
+                // Truyền giá trị vào thủ tục qua tham số
+                cmd.Parameters["@id"].Value = int.Parse(txtFoodID.Text);
+                cmd.Parameters["@name"].Value = txtName.Text;
+                cmd.Parameters["@unit"].Value = txtUnit.Text;
+                cmd.Parameters["@foodCategoryId"].Value = cbbCatName.SelectedValue;
+                cmd.Parameters["@price"].Value = nudPrice.Value;
+                cmd.Parameters["@notes"].Value = txtNotes.Text;
+
+                // Mở kết nối
+                conn.Open();
+
+                int numRowAffected = cmd.ExecuteNonQuery();
+
+                // Thông báo kết quả
+                if (numRowAffected > 0)
+                {
+                    MessageBox.Show("Successfully updating food", "Message");
+                    this.ResetText();
+                }
+                else
+                {
+                    MessageBox.Show("Updating food failed");
+                }
+
+                // Đóng kết nối
+                conn.Close();
+                conn.Dispose();
+            }
+            // Bắt lỗi SQL và các lỗi khác
+            catch (SqlException exception)
+            {
+                MessageBox.Show(exception.Message, "SQL Error");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ 2212484_MaiXuanTruong_Lab07/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2212484_MaiXuanTruong_Lab07
+{
+    public partial class Form1 : Form
+    {
+        private DataTable foodTable;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoadCategory()
+        {
+            string connectionString = "Data Source=PC819;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ID, Name FROM Category";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            conn.Open();
+            adapter.Fill(dt);
+            conn.Close();
+            conn.Dispose();
+            cbbCategory.DataSource = dt;
+            cbbCategory.DisplayMember = "Name";
+            cbbCategory.ValueMember = "ID";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.LoadCategory();
+        }
+
+        private void cbbCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbCategory.SelectedIndex == -1) return;
+            string ConnectionString = "Data Source=PC819;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Food WHERE FoodCategoryID = @categoryId";
+            cmd.Parameters.Add("@categoryId", SqlDbType.Int);
+
+            if (cbbCategory.SelectedValue is DataRowView)
+            {
+                DataRowView rowView = cbbCategory.SelectedValue as DataRowView;
+                cmd.Parameters["@categoryId"].Value = rowView["ID"];
+            }
+            else
+            {
+                cmd.Parameters["@categoryId"].Value = cbbCategory.SelectedValue;
+            }
+
+            // Tạo bộ điều phối dữ liệu
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable foodTable = new DataTable();
+
+            // Mở kết nối
+            conn.Open();
+
+            // Lấy dữ liệu từ csdl đưa vào DataTable
+            adapter.Fill(foodTable);
+
+            // Đóng kết nối và giải phóng bộ nhớ
+            conn.Close();
+            conn.Dispose();
+
+            // đưa dữ liệu vào data gridview
+            dgvFoodList.DataSource = foodTable;
+
+            // Tính số lượng và tên danh mục
+            lblQuantity.Text = foodTable.Rows.Count.ToString();
+            lblCatName.Text = cbbCategory.Text;
+        }
+
+        private void tsmCalculateQuantity_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=PC819;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT @numSaleFood = sum(Quantity) FROM BillDetails WHERE FoodID = @foodId";
+
+            // Lấy thông tin sản phẩm được chọn
+            if (dgvFoodList.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvFoodList.SelectedRows[0];
+                DataRowView rowView = selectedRow.DataBoundItem as DataRowView;
+
+                // Truyền tham số
+                cmd.Parameters.Add("@foodId", SqlDbType.Int);
+                cmd.Parameters["@foodId"].Value = rowView["ID"];
+
+                cmd.Parameters.Add("@numSaleFood", SqlDbType.Int);
+                cmd.Parameters["@numSaleFood"].Direction = ParameterDirection.Output;
+                // Mở kết nối csdl
+                conn.Open();
+
+                // Thực thi truy vấn và lấy dữ liệu từ tham số
+                cmd.ExecuteNonQuery();
+
+                string result = cmd.Parameters["@numSaleFood"].Value.ToString();
+                MessageBox.Show("Tổng số lượng món " + rowView["Name"] + " đã bán là: " + result + " " + rowView["Unit"]);
+
+                // Đóng kết nối csdl
+                conn.Close();
+            }
+            cmd.Dispose();
+            conn.Dispose();
+        }
+
+        private void tsmAddFood_Click(object sender, EventArgs e)
+        {
+            FoodInfoForm foodForm = new FoodInfoForm();
+            foodForm.FormClosed += new FormClosedEventHandler(foodForm_FormClose);
+            foodForm.Show(this);
+        }
+        private void tsmUpdateFood_Click(object sender, EventArgs e)
+        {
+            if(dgvFoodList.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvFoodList.SelectedRows[0];
+                DataRowView rowView = selectedRow?.DataBoundItem as DataRowView;
+
+                FoodInfoForm foodForm = new FoodInfoForm();
+                foodForm.FormClosed += new FormClosedEventHandler(foodForm_FormClose);
+
+                foodForm.Show(this);
+                foodForm.DisplayFoodID(rowView);
+            }
+        }
+
+        private void foodForm_FormClose(object sender, FormClosedEventArgs e)
+        {
+            int index = cbbCategory.SelectedIndex;
+            cbbCategory.SelectedIndex = -1;
+            cbbCategory.SelectedIndex = index;
+        }
+
+        private void txtSearchByName_TextChanged(object sender, EventArgs e)
+        {
+            if (foodTable == null) return;
+            string filterExpression = "Name like '%" + txtSearchByName.Text + "%'";
+            string sortExpression = "Price DESC";
+            DataViewRowState rowStateFilter = DataViewRowState.OriginalRows;
+
+            DataView foodView = new DataView(foodTable,filterExpression,sortExpression,rowStateFilter);
+
+            dgvFoodList.DataSource = foodView;
+        }
+    }
+}
